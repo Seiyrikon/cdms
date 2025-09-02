83dda229e0a43998c70eee27fba049a1aed36981
Seiryikon <rickygalpo7777@gmail.com>
2025-09-02 15:48:38 +0800

    first commit

---
+++ cdms_be/src/main/java/org/seiyrikon/cdms_be/CdmsBeApplication.java
@@ -0,0 +1,13 @@
+package org.seiyrikon.cdms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdmsBeApplication.class, args);
+	}
+
+}
---
+++ cdms_be/src/test/java/org/seiyrikon/cdms_be/CdmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seiyrikon.cdms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
