ac3dbf47991344b0957157bb022a571ed210e535
Seiryikon <rickygalpo7777@gmail.com>
2025-09-03 10:47:04 +0800

    Coding 2025/09/03 - Modified the generics to fix the JpaRepository error

--- cdms_be/src/main/java/org/seiyrikon/cdms_be/repository/EmployeeRepository.java
+++ cdms_be/src/main/java/org/seiyrikon/cdms_be/repository/EmployeeRepository.java
@@ -3,6 +3,6 @@ package org.seiyrikon.cdms_be.repository;
 import org.seiyrikon.cdms_be.domain.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Long, Employee>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
     
 }
