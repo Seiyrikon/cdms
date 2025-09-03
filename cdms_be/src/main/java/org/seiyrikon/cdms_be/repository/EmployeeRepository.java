73bbb60916882988a8e69367299ad03671f8f9b6
Seiryikon <rickygalpo7777@gmail.com>
2025-09-03 10:25:33 +0800

    Coding 2025/09/03 - Created repository for Employee

---
+++ cdms_be/src/main/java/org/seiyrikon/cdms_be/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package org.seiyrikon.cdms_be.repository;
+
+import org.seiyrikon.cdms_be.domain.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Long, Employee>{
+    
+}
