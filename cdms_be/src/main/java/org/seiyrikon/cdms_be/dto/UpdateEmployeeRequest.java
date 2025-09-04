1f748a73491b1d948d70935ac4b5c8a94020336c
Seiryikon <rickygalpo7777@gmail.com>
2025-09-04 14:59:31 +0800

    Coding 2025/09/04 - Added access modifier to UpdateEmployeeRequest String

--- cdms_be/src/main/java/org/seiyrikon/cdms_be/dto/UpdateEmployeeRequest.java
+++ cdms_be/src/main/java/org/seiyrikon/cdms_be/dto/UpdateEmployeeRequest.java
@@ -7,5 +7,5 @@ import lombok.Data;
 @Data
 public class UpdateEmployeeRequest {
     private Department department;
-    String employeeName, employeeEmail, employeePhone;
+    private String employeeName, employeeEmail, employeePhone;
 }
